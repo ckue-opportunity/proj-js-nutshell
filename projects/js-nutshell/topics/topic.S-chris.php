2f95797161924df5bd16d916ae429f6e98da2fae
Christoph Künzler <ckue-opportunity@gmail.com>
2021-08-31 12:18:13 +0200

    small layout adjustments

--- projects/js-nutshell/topics/topic.S-chris.php
+++ projects/js-nutshell/topics/topic.S-chris.php
@@ -25,9 +25,9 @@
         </div>
 
         <!-- EXAMPLE 1 -->
-        <div class="codeArea flex-container codeExample">
-            <div class="col-6 code"
-                 style="padding-right:0px; padding-left:0px; width:100%;">
+        <div class="codeArea flex-container codeExample mw-100">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 code px-auto">
                 <h4>Example 1: Variable Substitution</h4>
                 <pre class="language-css">
                     <code class="language-css">
@@ -41,19 +41,19 @@ let intro = `Let's introduce ${firstName} ${lastName} "officially"!`;
 console.log(intro);</code>
                 </pre>
             </div>
-            <div class="col-6 output"
-                 style="padding-right:0px; padding-left:0px; width:100%;">
-                <button onclick="S_templateStrings1();">Run</button>
-                <button onclick="clearResultText(1);">Reset</button>
+            <div class="col-md-6 col-sm-12 output px-auto">
+                <button class="btn" onclick="S_templateStrings1();">Run</button>
+                <button class="btn" onclick="clearResultText(1);">Reset</button>
                 
                 <p id="result-text-1" class="result-text">&nbsp;</p>
             </div>
         </div>
+        </div>
 
         <!-- EXAMPLE 2 -->
-        <div class="codeArea flex-container codeExample">
-            <div class="col-6 code"
-                 style="padding-right:0px; padding-left:0px; width:100%;">
+        <div class="codeArea flex-container codeExample mw-100">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 code px-auto">
                 <h4>Example 2: Expression Substitution</h4>
                 <pre class="language-css">
                     <code class="language-css">
@@ -67,10 +67,9 @@ let statement = `Twenty-one is ${a + b} and not ${2 * a + b}.`;
 console.log(statement);</code>
                 </pre>
             </div>
-            <div class="col-6 output" 
-                 style="padding-right:0px; padding-left:0px; width:100%;">
-                <button onclick="S_templateStrings2();">Run</button>
-                <button onclick="clearResultText(2);">Reset</button>
+            <div class="col-md-6 col-sm-12 output px-auto">
+                <button class="btn" onclick="S_templateStrings2();">Run</button>
+                <button class="btn" onclick="clearResultText(2);">Reset</button>
                 
                 <p id="result-text-2" class="result-text">&nbsp;</p>
 
@@ -78,26 +77,23 @@ console.log(statement);</code>
                 <h3>Related Topics</h3>
                 <p>Quotes Inside Strings, Multiline Strings, HTML Templates</p>
 
-                <h3>More Examples</h3>
+                <!--h3>More Examples</h3>
                 <a href="#">Template Strings - Mariano</a><br>
                 <a href="#">Template Strings - Caro</a>
-                <p style="height:1px;"></p>
+                <p style="height:1px;"></p-->
 
                 <h3>More Info</h3>
                 <a href="https://www.w3schools.com/js/js_string_templates.asp" target="_blank">String Templates - w3schools</a><br>
                 <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals" target="_blank">Template Strings - MDN Web Docs</a>
+                
                 <p style="height:1px;"></p>
 
             </div>
         </div>
+        </div>
 
     </div>
 
-    <div class="tableOfContent">
-        <ul>
-            <li><a href="#"></a>TOC Links</li>
-        </ul>
-    </div>
 </main>
 
 <script src="../js/topic.S-chris.js"></script>
